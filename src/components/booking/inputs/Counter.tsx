3b4a38d3653fd42974b90a65c2cbbfdd19e53b8c
Pavel Vondra <pavelvondra66@gmail.com>
2023-11-26 15:52:25 +0100

    fix: Fix the counter handling

--- src/components/booking/inputs/Counter.tsx
+++ src/components/booking/inputs/Counter.tsx
@@ -6,23 +6,22 @@ import plusIcon from "@images/icons/icon-plus.svg";
 
 const Counter = ({ onChange, onBlur, value }: FieldValues) => {
   const handleMinus = () => {
-    if (value === 0) return;
+    if (value === 1) return;
     onChange((prev: number) => prev - 1);
   };
   const handlePlus = () => {
-    if (value === 0) return;
     onChange((prev: number) => prev + 1);
   };
 
   return (
     <div className="formElement flex justify-between px-12">
-      <button onClick={handleMinus}>
+      <button type="button" onClick={handleMinus}>
         <img src={minusIcon.src} alt="Minus icon" />
       </button>
       <div className="flex font-bold">
         <span>{value} people</span>
       </div>
-      <button onClick={handlePlus}>
+      <button type="button" onClick={handlePlus}>
         <img src={plusIcon.src} alt="Plus icon" />
       </button>
     </div>
